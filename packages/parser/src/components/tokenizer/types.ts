fa2a43c146de22724182d845e0693b9102d6f40a
Aniket Singh <aniketsingh0104@gmail.com>
2022-01-01 18:49:16 +0530

    Feat: Add tokenizer

---
+++ packages/parser/src/components/tokenizer/types.ts
@@ -0,0 +1,15 @@
+export interface Tokenizer {
+  initTokenizer(stringToTokenize: String): void;
+
+  isEOF(): boolean;
+
+  hasMoreTokens(): boolean;
+
+  getNextToken(): Token | null;
+}
+
+export interface Token {
+    type: string;
+
+    value: string;
+}
---
+++ packages/parser/src/constants/bhaiLangSpec.ts
@@ -0,0 +1,42 @@
+export const SPEC = [
+    // Whitespcaes
+    {regex: /^\s+/, tokenType: null},
+
+    // singke line Comments
+    {regex: /^\/\/.*/, tokenType: null},
+
+    // multi line comments
+    {regex: /^\/\*[\s\S]*?\*\//, tokenType: null},
+
+    // Symbols, delimiters
+    {regex: /^;/, tokenType:';'},
+    {regex: /^\{/, tokenType: '{'},
+    {regex: /^\}/, tokenType: '}'},
+    {regex: /^\(/, tokenType: '('},
+    {regex: /^\)/, tokenType: ')'},
+    {regex: /^,/, tokenType:','},
+
+    //Keywords
+    {regex: /^\blet\b/, tokenType: 'let'},
+
+    // Number
+    {regex: /^\d+/, tokenType: 'NUMBER'},
+
+    // Identifier
+    {regex: /^\w+/, tokenType: 'IDENTIFIER'},
+
+
+    // Assignment operators: =, *=, /=, +=, -=
+    {regex: /^=/, tokenType: 'SIMPLE_ASSIGN'},
+    {regex: /^[\*\/\+\-]=/, tokenType: 'COMPLEX_ASSIGN'},
+
+    // operator
+    {regex: /^[+\-]/, tokenType: 'ADDITIVE_OPERATOR'},
+    {regex: /^[*\/]/, tokenType: 'MULTIPLICATIVE_OPERATOR'},
+
+    // String
+    {regex: /^"[^"]*"/, tokenType: 'STRING'},
+    {regex: /^'[^']*'/, tokenType: 'STRING'}
+];
+
+export type Spec = typeof SPEC
