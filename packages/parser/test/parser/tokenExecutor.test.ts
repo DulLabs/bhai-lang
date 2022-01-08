981755cb83a646494e3ba8e31a907fb44d687f1b
Rishabh Tripathi <ristripathi97@gmail.com>
2022-01-09 01:41:58 +0530

    fix: fix token executor test

--- packages/parser/test/parser/tokenExecutor.test.ts
+++ packages/parser/test/parser/tokenExecutor.test.ts
@@ -1,19 +1,19 @@
 import TokenExecutor from '../../src/components/parser/tokenExecutor';
+import { Tokenizer } from '../../src/components/tokenizer/types';
 import { TokenTypes } from '../../src/constants/bhaiLangSpec';
 
-const getNextTokenMock = jest.fn().mockImplementation(() => {
+
+const mockGetNextToken = jest.fn().mockImplementation(() => {
     return null;
 });
 
-jest.mock("../../src/components/tokenizer", () => {
-    return {
-        Tokenizer: jest.fn().mockImplementation(() => {
-            return {
-                getNextToken: getNextTokenMock
-            }
-        })
-    };
-});
+const tokenizer: jest.Mocked<Tokenizer> = {
+    getNextToken: mockGetNextToken,
+    initTokenizer: jest.fn(),
+    isEOF: jest.fn(),
+    hasMoreTokens: jest.fn(),
+}
+
 
 test("test eatTokenAndForwardLookahead", () => {
 
@@ -22,13 +22,12 @@ test("test eatTokenAndForwardLookahead", () => {
       value: "hi bhai",
     }
 
-    //@ts-ignore
-    const tokenExecutor = new TokenExecutor();
+    const tokenExecutor = new TokenExecutor(tokenizer);
 
     tokenExecutor.setLookahead(lookahead);
 
     expect(tokenExecutor.eatTokenAndForwardLookahead(TokenTypes.HI_BHAI_TYPE)).toStrictEqual({
-        type: "",
-        value: ""
+        type: TokenTypes.HI_BHAI_TYPE,
+        value: "hi bhai"
     });
   });
