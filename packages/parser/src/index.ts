85c47deecc4c10a655345cd94f319d4dee5571cb
Rishabh Tripathi <ristripathi97@gmail.com>
2022-01-16 01:46:09 +0530

    feat: add parcel for bundling

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,7 +1,9 @@
-import BhaiLangModule from './src/module/bhaiLangModule';
+import BhaiLangModule from './module/bhaiLangModule';
 
 
 const parser = BhaiLangModule.getParser();
 console.log(JSON.stringify(parser.parse(`
 hi bhai
  bye bhai`)));
+
+export default parser;
