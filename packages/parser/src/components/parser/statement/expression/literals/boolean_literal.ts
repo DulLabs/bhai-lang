49dfdab88853403c0dc00f3a1079d8c1983021e1
Aniket Singh <aniketsingh0104@gmail.com>
2022-01-17 23:22:54 +0530

    refact: basic format

--- packages/parser/src/components/parser/statement/expression/literals/boolean_literal.ts
+++ packages/parser/src/components/parser/statement/expression/literals/boolean_literal.ts
@@ -1,15 +1,15 @@
-import Literal from '.';
+import Literal from ".";
 
-import { TokenTypes } from '../../../../../constants/bhaiLangSpec';
-
+import { TokenTypes } from "../../../../../constants/bhaiLangSpec";
 
 export default class BooleanLiteral extends Literal {
   getLiteral() {
-    const token = this._tokenExecutor.eatTokenAndForwardLookahead(TokenTypes.BOOLEAN_TYPE);
-    return { 
-      type: 'BooleanLiteral',
-      value: token.value
-      }
+    const token = this._tokenExecutor.eatTokenAndForwardLookahead(
+      TokenTypes.BOOLEAN_TYPE
+    );
+    return {
+      type: "BooleanLiteral",
+      value: token.value,
+    };
   }
-  
 }
