80d40bc022b5fb275143e43338b8540d604ab517
Rishabh Tripathi <ristripathi97@gmail.com>
2022-01-22 00:08:19 +0530

    feat: add block statement

--- packages/interpreter/src/components/visitor/initStatement.ts
+++ packages/interpreter/src/components/visitor/initStatement.ts
@@ -7,7 +7,7 @@ import InterpreterModule from "../../module/interpreterModule";
 export default class InitStatement extends Visitor {
   visitNode(node: ASTNode) {
     if (Array.isArray(node.body)) {
-      node.body?.forEach((statement: any) => {
+      node.body?.forEach((statement: ASTNode) => {
         InterpreterModule.getVisitor(statement.type)?.visitNode(statement);
       });
     }
--- packages/interpreter/src/module/interpreterModule.ts
+++ packages/interpreter/src/module/interpreterModule.ts
@@ -1,6 +1,8 @@
 import { NodeType } from "bhai-lang-parser";
 
+import Scope from "../components/scope";
 import Visitor from "../components/visitor";
+import BlockStatement from "../components/visitor/blockStatement";
 import BooleanLiteral from "../components/visitor/booleanLiteral";
 import EmptyStatement from "../components/visitor/emptyStatement";
 import InitStatement from "../components/visitor/initStatement";
@@ -16,15 +18,27 @@ export default class InterpreterModule {
     [NodeType.InitStatement]: new InitStatement(),
     [NodeType.PrintStatement]: new PrintStatement(),
     [NodeType.EmptyStatement]: new EmptyStatement(),
+    [NodeType.BlockStatement]: new BlockStatement(),
     [NodeType.StringLiteral]: new StringLiteral(),
     [NodeType.NumericLiteral]: new NumericLiteral(),
     [NodeType.BooleanLiteral]: new BooleanLiteral(),
   } as Record<string, Visitor>;
+
+  private static _currentScope: Scope;
 
   static getVisitor(nodeType: string) {
     const visitor = InterpreterModule._visitorMap[nodeType];
     if (visitor) {
       return visitor;
     }
+  }
+
+  static getCurrentScope() {
+    this._currentScope = this._currentScope || new Scope(null);
+    return this._currentScope;
+  }
+
+  static setCurrentScope(scope: Scope) {
+    this._currentScope = scope;
   }
 }
