e1ab8d62b44beac2c072ca706a7fae830a958a9e
Rishabh Tripathi <ristripathi97@gmail.com>
2022-01-21 19:06:13 +0530

    feat: bundle using tsup (#24)
    * feat: bundle using tsup
    
    * fix: add missing dependency
    
    Co-authored-by: Aniket Singh <aniketsingh0104@gmail.com>

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,14 +1,4 @@
-import BhaiLangModule from "./module/bhaiLangModule";
+import BhaiLangModule from './module/bhaiLangModule';
 
-const parser = BhaiLangModule.getParser();
-console.log(
-  JSON.stringify(
-    parser.parse(`
-    hi bhai
-    a *= 4;
-    bye bhai`)
-  )
-);
 
-export { NodeType } from "./constants/constants";
-export default parser;
+export default BhaiLangModule.getParser().parse;
