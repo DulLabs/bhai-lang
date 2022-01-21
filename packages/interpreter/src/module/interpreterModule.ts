0e5d9e02edec8dd6822dd2ba2fd51ebf5020a820
Rishabh Tripathi <ristripathi97@gmail.com>
2022-01-21 23:43:58 +0530

    feat: handle literals

--- packages/interpreter/src/module/interpreterModule.ts
+++ packages/interpreter/src/module/interpreterModule.ts
@@ -1,9 +1,12 @@
 import { NodeType } from "bhai-lang-parser";
 
+import BooleanLiteral from "../components/visitor/booleanLiteral";
 import InitStatement from "../components/visitor/initStatement";
+import NumericLiteral from "../components/visitor/numericLiteral";
 import PrintStatement from "../components/visitor/printStatement";
 import Program from "../components/visitor/program";
 import StringLiteral from "../components/visitor/stringLiteral";
+
 
 export default class InterpreterModule {
   private static _visitorMap = {
@@ -11,6 +14,8 @@ export default class InterpreterModule {
     [NodeType.InitStatement]: new InitStatement(),
     [NodeType.PrintStatement]: new PrintStatement(),
     [NodeType.StringLiteral]: new StringLiteral(),
+    [NodeType.NumericLiteral]: new NumericLiteral(),
+    [NodeType.BooleanLiteral]: new BooleanLiteral(),
   } as Record<string, any>;
 
   static getVisitor(nodeType: string) {
