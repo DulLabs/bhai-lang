6a24cabeb1eeb463c47210dddfb59ed224970ea4
Aniket Singh <aniketsingh0104@gmail.com>
2022-01-22 19:18:30 +0530

    fix: file naming

--- packages/parser/src/components/parser/statement/variableStatement.ts
+++ packages/parser/src/components/parser/statement/variableStatement.ts
@@ -6,7 +6,7 @@ import TokenExecutor from "../tokenExecutor";
 import { ASTNode } from "../types/nodeTypes";
 
 import Expression from "./expression";
-import NullLiteral from "./expression/literals/null_literal";
+import NullLiteral from "./expression/literals/nullLiteral";
 
 
 export default class VariableStatement extends Statement {
