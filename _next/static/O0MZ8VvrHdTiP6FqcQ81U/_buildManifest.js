0d66c40588df950a81788fc8f30d6316a28b4d88
aniketsingh0104 <aniketsingh0104@users.noreply.github.com>
2022-02-25 12:06:51 +0000

    deploy: 8c6f7471402141a20da9438b86337555ae4755d8

--- _next/static/O0MZ8VvrHdTiP6FqcQ81U/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/279-48b3ae63d9ce8380.js","static/css/afa779690db171c4.css","static/chunks/pages/index-14e6af18c356cbb1.js"],"/_error":["static/chunks/pages/_error-d20da8f9a50c916f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
