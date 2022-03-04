7cba4cb7bf1a52079ee58a6b15aa228b7b90bc1d
ristri <ristri@users.noreply.github.com>
2022-03-04 18:13:35 +0000

    deploy: c552f8d57e77fe0bdc1b92acbad3babe0ead505e

---
+++ _next/static/M1xIB281rt3xyZwVtc9L-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/279-babc1687b58e9823.js","static/css/afa779690db171c4.css","static/chunks/pages/index-5063d45c3bdf6643.js"],"/_error":["static/chunks/pages/_error-d20da8f9a50c916f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
