005ccd3e63d150c041be905fee917bc7c9beabdd
Rishabh Tripathi <ristripathi97@gmail.com>
2022-03-05 00:15:00 +0530

    fix: lighthouse changes

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,11 +1,23 @@
+import Head from "next/head";
+
 import Code from "../components/Code";
 import Documentation from "../components/Documentation";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
+
 export default function Docs() {
   return (
     <div className="bg-background">
+      <Head>
+        <title>Bhailang Documentation</title>
+        <meta property="og:title" content="Bhailang Documentation" key="title" />
+        <meta property="og:type" content="website" key="type" />
+        <meta property="og:url" content="https://bhailang.js.org" key="url" />
+        <meta property="og:description" content="Bhailang is dynamically typed toy programming language, based on an inside joke, written in Typescript. Created by Aniket Singh and Rishabh Tripathi." key="description" />
+        <meta name="description" content="Bhailang is dynamically typed toy programming language, based on an inside joke, written in Typescript. Created by Aniket Singh and Rishabh Tripathi." />
+        <meta property="og:site_name" content="Bhailang Documentation" key="siteName" />
+      </Head>
       <Header />
       <Code />
       <Documentation />
