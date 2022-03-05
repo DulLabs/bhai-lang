e0bac735b2a6d3c4e372b6c39fe344bdac30499c
Rishabh Tripathi <ristripathi97@gmail.com>
2022-03-06 00:40:07 +0530

    fix: footer padding

--- apps/docs/components/Footer/index.tsx
+++ apps/docs/components/Footer/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
+
 const Footer = (props: Props) => {
   const {} = props;
   return (
     <footer>
-      <div className="text-white text-sm text-center">
+      <div className="text-white text-sm text-center py-4">
         &copy; 2022{" "}
         <a className="hover:text-bhagwa-600" href="https://tripathi.dev">
           Rishabh Tripathi
