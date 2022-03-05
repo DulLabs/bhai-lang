2210740b5260f55936fc7a6ebfabc9b385e8a5db
Rishabh Tripathi <ristripathi97@gmail.com>
2022-03-06 00:45:35 +0530

    fix: Error message

--- packages/interpreter/src/exceptions/runtimeException.ts
+++ packages/interpreter/src/exceptions/runtimeException.ts
@@ -2,6 +2,6 @@ export default class RuntimeException extends Error {
   constructor(errorMessage: string) {
     super(errorMessage);
     this.name = "RuntimeException";
-    this.message = `Are bhai bhai bhai.... : ${errorMessage}`;
+    this.message = `${errorMessage}`;
   }
 }
