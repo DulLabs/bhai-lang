30dbe79636ee3fd6e445534499bad227793cd5b9
Rishabh Tripathi <ristripathi97@gmail.com>
2022-03-05 14:32:30 +0530

    feat: add posthog (#56)
    * feat: add posthog
    
    * fix: move posthog init to _app.tsx

---
+++ apps/docs/helpers/index.ts
@@ -0,0 +1,9 @@
+import type Properties from "posthog-js";
+import posthog from "posthog-js";
+
+
+export const sendEvents = (eventName: string, properties: Properties) => {
+  if (process.env.NODE_ENV !== "development") {
+    posthog.capture(eventName, properties);
+  }
+}
--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -25,3 +25,4 @@ export default function Docs() {
     </div>
   );
 }
+
