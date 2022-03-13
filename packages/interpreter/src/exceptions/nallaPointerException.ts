353f376b32147c31111702672c6988f48ae95aa4
Aniket Singh <aniketsingh0104@gmail.com>
2022-03-13 11:38:19 +0530

    fix: change exception construtor name to strings

--- packages/interpreter/src/exceptions/nallaPointerException.ts
+++ packages/interpreter/src/exceptions/nallaPointerException.ts
@@ -1,7 +1,9 @@
 export default class NallaPointerException extends Error {
   constructor(errorMessage: string) {
+    const errorName = "NallaPointerException";
+    errorMessage = errorName + ": " + errorMessage;
     super(errorMessage);
-    this.name = this.constructor.name;
+    this.name = errorName;
     this.message = errorMessage;
   }
 }
