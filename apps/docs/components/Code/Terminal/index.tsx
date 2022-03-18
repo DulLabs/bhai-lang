1c81a54aaa53ee1653d111eb98f7f0f8062d4460
Ritik Banger <ritik@biz4group.com>
2022-03-18 20:30:49 +0530

    Error Message update issue- #197

--- apps/docs/components/Code/Terminal/index.tsx
+++ apps/docs/components/Code/Terminal/index.tsx
@@ -1,26 +1,26 @@
 import React, { useEffect, useRef } from "react";
 
-
 const Terminal = (props: Props) => {
   const { output, isSuccess } = props;
   const terminalRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => { 
+  useEffect(() => {
     if (output.length) {
       setTimeout(() => terminalRef.current?.scrollIntoView(false), 100);
     }
   }, [output]);
 
-  
   return (
     <div
-    ref={terminalRef}
+      ref={terminalRef}
       className={`${
         isSuccess !== null ? "terminal" : "terminal-collapsed"
       } bg-black text-white my-6`}
     >
       {isSuccess !== null && !isSuccess ? (
-        <div className="text-red-700 output opacity-0">❌ Arre Bhai Bhai Bhai !!!</div>
+        <div className="text-red-700 output opacity-0">
+          ❌ Arre Bhai Bhai Bhai, Hag Diya !!!
+        </div>
       ) : (
         <div className="text-green-700 output opacity-0">Shandaar bhai 🎉</div>
       )}
