24d926fc955432b5d45cc16fb80f4478797ef5c6
Harsh Kanjariya <code.harshkanjariya@gmail.com>
2022-03-20 08:09:36 +0530

    fix: non-boolean if conditions + tests

--- packages/interpreter/src/components/visitor/ifStatement.ts
+++ packages/interpreter/src/components/visitor/ifStatement.ts
@@ -11,19 +11,19 @@ export default class IfStatement implements Visitor {
     const parentScope = InterpreterModule.getCurrentScope();
     if (test) {
       const testResult = InterpreterModule.getVisitor(test.type).visitNode(test);
-      if (testResult === true || testResult === "sahi") {
-        const consequent = node.consequent;
-        if (consequent) {
-          InterpreterModule.setCurrentScope(new Scope(parentScope));
-          InterpreterModule.getCurrentScope().setLoop(parentScope.isLoop());
-           InterpreterModule.getVisitor(consequent.type).visitNode(consequent);
-        }
-      } else {
+      if (testResult === "galat" || testResult === "nalla" || !testResult) {
         const alternate = node.alternate;
         if (alternate) {
           InterpreterModule.setCurrentScope(new Scope(parentScope));
           InterpreterModule.getCurrentScope().setLoop(parentScope.isLoop());
           InterpreterModule.getVisitor(alternate.type).visitNode(alternate);
+        }
+      } else {
+        const consequent = node.consequent;
+        if (consequent) {
+          InterpreterModule.setCurrentScope(new Scope(parentScope));
+          InterpreterModule.getCurrentScope().setLoop(parentScope.isLoop());
+          InterpreterModule.getVisitor(consequent.type).visitNode(consequent);
         }
       }
     } 
