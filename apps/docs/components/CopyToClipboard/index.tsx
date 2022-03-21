b3288d7fba62147833e926c661de13c71f1faea1
Shubham Singh <shubham.singh1@spinny.com>
2022-03-21 11:45:07 +0530

    Added logic to hide the copy to clipboard button only on medium viewports and above

--- apps/docs/components/CopyToClipboard/index.tsx
+++ apps/docs/components/CopyToClipboard/index.tsx
@@ -34,13 +34,13 @@ export default function CopyToClipboard(props: Props) {
     <>
       <button
         ref={buttonRef}
-        className={"hidden absolute top-2 right-2 sm:top-4 sm:right-4 group-hover:flex items-center justify-center p-1 sm:p-2 border border-[#f0f6fc1a] text-base font-medium rounded-md text-gray-400 bg-[#333] hover:bg-[#4d4d4d] cursor-pointer transition-all duration-100 " + (copySuccess ? 'text-bhagwa-300 border-bhagwa-300' : '')}
+        className={"md:hidden absolute top-2 right-2 sm:top-4 sm:right-4 group-hover:flex items-center justify-center p-1 sm:p-2 border border-[#f0f6fc1a] text-base font-medium rounded-md text-gray-400 bg-[#333] hover:bg-[#4d4d4d] cursor-pointer transition-all duration-100 " + (copySuccess ? 'text-bhagwa-300 border-bhagwa-300' : '')}
         onClick={copyToClipboard}
       >
         {copySuccess ?
           <>
             <TickIcon />
-            <span className="copied-text absolute right-[calc(100%+8px)] p-1 rounded-md text-xs font-normal text-bhagwa-300 bg-[#666] border border-transparent">
+            <span className="copied-text absolute top-1/2 -translate-y-1/2 right-[calc(100%+8px)] p-1 rounded-md text-xs font-normal text-bhagwa-300 bg-[#666] border border-transparent">
               Copied!
             </span>
           </>
