e210a2562b5901ccc6fe57c4236bb36b72d8ac5c
ItsBhuvnesh <codeiseasy94@gmail.com>
2022-03-24 12:49:53 +0530

    🐛 Fixed Bugs

--- packages/interpreter/typings/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "bhai-lang-interpreter" {
-  function interpret(code: string): void;
-
-  declare const interpreter = {
-    interpret: interpret,
-  };
-
-  export default interpreter;
-}
--- packages/parser/typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "bhai-lang-parser" {
-  declare const parser = {};
-
-  export default parser;
-}
