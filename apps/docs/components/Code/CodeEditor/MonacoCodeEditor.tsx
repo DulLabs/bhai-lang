36ae43819d0fee4b96fe0752910719ab7d59b5ee
gitwikc <sahusattwik@gmail.com>
2022-03-26 12:16:45 +0000

    Syntax tokenizer for Bhailang

--- apps/docs/components/Code/CodeEditor/MonacoCodeEditor.tsx
+++ apps/docs/components/Code/CodeEditor/MonacoCodeEditor.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import { OnChange } from "@monaco-editor/react";
 import monacoOptions from "./monacoOptions";
 import Editor from "@monaco-editor/react";
+import monaco from "monaco-editor";
+import { bhailangConfig, bhailangSyntax } from "./monacoBhailang";
+import { useMonaco } from "@monaco-editor/react";
 
 interface Props {
   handleChange: OnChange;
@@ -9,11 +12,20 @@ interface Props {
 }
 
 const MonacoCodeEditor = (props: Props) => {
+  const monaco = useMonaco()!;
+  React.useEffect(() => {
+    if (monaco) {
+      monaco.languages.register({ id: "bhailang" });
+      monaco.languages.setMonarchTokensProvider("bhailang", bhailangSyntax);
+      monaco.languages.setLanguageConfiguration("bhailang", bhailangConfig);
+    }
+  }, [monaco]);
+
   return (
     <div className="shadow-md">
       <Editor
         height="50vh"
-        language="javascript"
+        language="bhailang"
         onChange={props.handleChange}
         value={props.code}
         options={monacoOptions}
--- apps/docs/components/Code/CodeEditor/monacoBhailang.ts
+++ apps/docs/components/Code/CodeEditor/monacoBhailang.ts
@@ -1,5 +1,230 @@
-import { editor } from "monaco-editor";
+import monaco from "monaco-editor";
 
-const bhailangSyntax = {};
+const bhailangKeywords =
+  "hi|bye|bol|bhai|agar|nahi|to|warna|jab|tak|bas|kar|bhai|agla|dekh".split(
+    "|"
+  );
 
-export default bhailangSyntax;
+export const bhailangSyntax: monaco.languages.IMonarchLanguage = {
+  // Set defaultToken to invalid to see what you do not tokenize yet
+  defaultToken: "invalid",
+  tokenPostfix: ".bhai",
+
+  keywords: bhailangKeywords,
+
+  typeKeywords: ["any", "boolean", "number", "object", "string", "nalla"],
+
+  operators: [
+    "<=",
+    ">=",
+    "==",
+    "!=",
+    "===",
+    "!==",
+    "=>",
+    "+",
+    "-",
+    "**",
+    "*",
+    "/",
+    "%",
+    "++",
+    "--",
+    "<<",
+    "</",
+    ">>",
+    ">>>",
+    "&",
+    "|",
+    "^",
+    "!",
+    "~",
+    "&&",
+    "||",
+    "?",
+    ":",
+    "=",
+    "+=",
+    "-=",
+    "*=",
+    "**=",
+    "/=",
+    "%=",
+    "<<=",
+    ">>=",
+    ">>>=",
+    "&=",
+    "|=",
+    "^=",
+    "@",
+  ],
+
+  // we include these common regular expressions
+  symbols: /[=><!~?:&|+\-*\/\^%]+/,
+  escapes:
+    /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+  digits: /\d+(_+\d+)*/,
+  octaldigits: /[0-7]+(_+[0-7]+)*/,
+  binarydigits: /[0-1]+(_+[0-1]+)*/,
+  hexdigits: /[[0-9a-fA-F]+(_+[0-9a-fA-F]+)*/,
+
+  regexpctl: /[(){}\[\]\$\^|\-*+?\.]/,
+  regexpesc:
+    /\\(?:[bBdDfnrstvwWn0\\\/]|@regexpctl|c[A-Z]|x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4})/,
+
+  // The main tokenizer for our languages
+  tokenizer: {
+    root: [[/[{}]/, "delimiter.bracket"], { include: "common" }],
+
+    common: [
+      // identifiers and keywords
+      [
+        /[a-z_$][\w$]*/,
+        {
+          cases: {
+            "@typeKeywords": "keyword",
+            "@keywords": "keyword",
+            "@default": "identifier",
+          },
+        },
+      ],
+      [/[A-Z][\w\$]*/, "type.identifier"], // to show class names nicely
+      // [/[A-Z][\w\$]*/, 'identifier'],
+
+      // whitespace
+      { include: "@whitespace" },
+
+      // regular expression: ensure it is terminated before beginning (otherwise it is an opeator)
+      [
+        /\/(?=([^\\\/]|\\.)+\/([gimsuy]*)(\s*)(\.|;|\/|,|\)|\]|\}|$))/,
+        { token: "regexp", bracket: "@open", next: "@regexp" },
+      ],
+
+      // delimiters and operators
+      [/[()\[\]]/, "@brackets"],
+      [/[<>](?!@symbols)/, "@brackets"],
+      [
+        /@symbols/,
+        {
+          cases: {
+            "@operators": "delimiter",
+            "@default": "",
+          },
+        },
+      ],
+
+      // numbers
+      [/(@digits)[eE]([\-+]?(@digits))?/, "number.float"],
+      [/(@digits)\.(@digits)([eE][\-+]?(@digits))?/, "number.float"],
+      [/0[xX](@hexdigits)/, "number.hex"],
+      [/0[oO]?(@octaldigits)/, "number.octal"],
+      [/0[bB](@binarydigits)/, "number.binary"],
+      [/(@digits)/, "number"],
+
+      // delimiter: after number because of .\d floats
+      [/[;,.]/, "delimiter"],
+
+      // strings
+      [/"([^"\\]|\\.)*$/, "string.invalid"], // non-teminated string
+      [/'([^'\\]|\\.)*$/, "string.invalid"], // non-teminated string
+      [/"/, "string", "@string_double"],
+      [/'/, "string", "@string_single"],
+      [/`/, "string", "@string_backtick"],
+    ],
+
+    whitespace: [
+      [/[ \t\r\n]+/, ""],
+      [/\/\*\*(?!\/)/, "comment.doc", "@jsdoc"],
+      [/\/\*/, "comment", "@comment"],
+      [/\/\/.*$/, "comment"],
+    ],
+
+    comment: [
+      //   [/[^\/*]+/, "comment"],
+      [/((\n*.*)*)(.*)hi bhai/, "comment"],
+      [/\*\//, "comment", "@pop"],
+      [/[\/*]/, "comment"],
+    ],
+
+    jsdoc: [
+      [/[^\/*]+/, "comment.doc"],
+      [/\*\//, "comment.doc", "@pop"],
+      [/[\/*]/, "comment.doc"],
+    ],
+
+    // We match regular expression quite precisely
+    regexp: [
+      [
+        /(\{)(\d+(?:,\d*)?)(\})/,
+        [
+          "regexp.escape.control",
+          "regexp.escape.control",
+          "regexp.escape.control",
+        ],
+      ],
+      [/(\()(\?:|\?=|\?!)/, ["regexp.escape.control", "regexp.escape.control"]],
+      [/[()]/, "regexp.escape.control"],
+      [/@regexpctl/, "regexp.escape.control"],
+      [/[^\\\/]/, "regexp"],
+      [/@regexpesc/, "regexp.escape"],
+      [/\\\./, "regexp.invalid"],
+    ],
+
+    regexrange: [
+      [/-/, "regexp.escape.control"],
+      [/\^/, "regexp.invalid"],
+      [/@regexpesc/, "regexp.escape"],
+      [/[^\]]/, "regexp"],
+      [
+        /\]/,
+        { token: "regexp.escape.control", next: "@pop", bracket: "@close" },
+      ],
+    ],
+
+    string_double: [
+      [/[^\\"]+/, "string"],
+      [/@escapes/, "string.escape"],
+      [/\\./, "string.escape.invalid"],
+      [/"/, "string", "@pop"],
+    ],
+
+    string_single: [
+      [/[^\\']+/, "string"],
+      [/@escapes/, "string.escape"],
+      [/\\./, "string.escape.invalid"],
+      [/'/, "string", "@pop"],
+    ],
+
+    string_backtick: [
+      [/\$\{/, { token: "delimiter.bracket", next: "@bracketCounting" }],
+      [/[^\\`$]+/, "string"],
+      [/@escapes/, "string.escape"],
+      [/\\./, "string.escape.invalid"],
+      [/`/, "string", "@pop"],
+    ],
+
+    bracketCounting: [
+      [/\{/, "delimiter.bracket", "@bracketCounting"],
+      [/\}/, "delimiter.bracket", "@pop"],
+      { include: "common" },
+    ],
+  },
+};
+
+export const bhailangConfig: monaco.languages.LanguageConfiguration = {
+  surroundingPairs: [
+    { open: "{", close: "}" },
+    { open: "[", close: "]" },
+    { open: "(", close: ")" },
+    { open: "<", close: ">" },
+    { open: "'", close: "'" },
+    { open: '"', close: '"' },
+  ],
+  autoClosingPairs: [
+    { open: "{", close: "}" },
+    { open: "[", close: "]" },
+    { open: "(", close: ")" },
+    { open: "'", close: "'", notIn: ["string", "comment"] },
+    { open: '"', close: '"', notIn: ["string", "comment"] },
+  ],
+};
